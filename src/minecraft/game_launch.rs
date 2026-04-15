9abc9985eb02684a30f071f4f3309328d13dcb9b
Suverent-Shiro <196466050+Suverent-Shiro@users.noreply.github.com>
2026-04-15 19:45:55 +0200

    15-04-2026

--- src/minecraft/game_launch.rs
+++ src/minecraft/game_launch.rs
@@ -1,11 +1,18 @@
-// This is where the game is launched.
+// // This is where the game is launched.
 
-// Copper Launcher is licensed under GNU General Public License v3.0.
-// Copyright (c) 2026 Suverent_Shiro
-//
-// See LICENSE file for more details.
+// // Copper Launcher is licensed under GNU General Public License v3.0.
+// // Copyright (c) 2026 Suverent_Shiro
+// //
+// // See LICENSE file for more details.
+
+// use std::os::unix::process;
 
 
-fn check_java() {
-    
-}
+// fn check_java() {
+//     let java_dir = "/usr/lib/jvm/java-21-openjdk/bin/java";
+// }
+
+// fn launch_jvm() {
+//     let java_dir
+//     process
+// }
